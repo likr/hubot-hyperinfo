9bbc77ccecc105d582cf3c193c599b12b050d042
Yosuke ONOUE <onoue@likr-lab.com>
2015-06-18 08:43:42 +0900

    Add help and remove plugins

--- scripts/hyperinfo.js
+++ scripts/hyperinfo.js
@@ -1,5 +1,10 @@
 // Description:
 //   HyperInfo remote control.
+//
+// Commands:
+//   hubot connect <ip> <port> - Connect bot to HyperInfoWall.
+//   hubot disconnect - Disconnect bot from HyperInfoWall.
+//   hubot open <url> - Open web page in HyperInfoWall.
 
 require("babel/register");
 module.exports = function (robot) {
